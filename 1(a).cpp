401ee603df207e714e898ed9f66919010cc0dac0
DanilaPonomarev <96246197+DanilaPonomarev@users.noreply.github.com>
2021-12-18 12:14:51 +0300

    Create 1(a).cpp

---
+++ 1(a).cpp
@@ -0,0 +1,16 @@
+a) заменить все символы ‘?’ на’!’;
+#include <stdio.h>
+int main()
+{
+    int a;
+    while (a != EOF && a != '.') 
+    {
+        a = getchar();
+        if (a == '?') 
+        {
+            putchar('!');
+            continue;
+        }
+        putchar(a);
+    }
+}
